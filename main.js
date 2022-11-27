296b8279adcdba4fd0b2287185e27c955e24b28c
Tora <109001487+salamovav@users.noreply.github.com>
2022-11-27 22:59:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// let i=1;
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=50)
+
+//  let i=50;
+//  do{
+//     console.log(i);
+//     i--
+// }while(i>=25)
+
+for (let i=2500;i<=3000;i++){
+    if(i%2 !=0){
+        console.log(i);
+    }
+}
